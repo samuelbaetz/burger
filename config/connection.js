042bcd78335a00833d16bd2242f880c9d0291cba
Samuel Baetz <samuelbaetz105@gmail.com>
2020-12-03 22:53:12 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 var Sequelize = require("sequelize")
 
-var sequelize = new Sequelize("burgers_db", "postgres", "root",{
-    host: "localhost",
-    port: 5432,
-    dialect: 'postgres',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
-});
+// var sequelize = new Sequelize("burgers_db", "postgres", "root",{
+//     host: "localhost",
+//     port: 5432,
+//     dialect: 'postgres',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         idle: 10000
+//     }
+// });
+
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  });
 
 module.exports = sequelize
